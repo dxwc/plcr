6fb19ecf7831b8ae1edd84cd898049d20989699c
dxwc <dxwc@users.noreply.github.com>
2018-12-30 21:29:35 -0500

    rewriten with more url suport, npm 1.0.0

--- README.md
+++ README.md
@@ -1,57 +1,36 @@
-[ Use from `npm`, Repository/master is currently a construction site ]
+A simple command line program to generate youtube playlist given video URLs
 
-A simple script to generate youtube playlist link given video urls (no youtube account needed)
+# Why
 
-# Setup
+You don't have to have a google account to make video playlist
 
+# Install
 
-1. Have [Node.js](https://nodejs.org/en/) installed (any version
-   higher than 6 should be fine)
-
-### Option 1
-
-2. Save [plcr.js](https://raw.githubusercontent.com/dxwc/plcr/master/plcr.js) and use it as a script
-    + Example A: On linux, `cd` to saved and run `chmod u+x plcr.js`, it will now be available for use from the directory by name `./plcr.js`
-    + Example B: `cd` in saved dir and run directly with node, ex: `node plcr.js`
-
-### Option 2
-
-2. Run command `npm install -g plcr` to install it from npm globally, `plcr` will be
-   available for use from any directory
-    + To update to latest version anytime after installation, re-run above command
-    + To uninstall, run `npm uninstall -g plcr`
-    + _Unix may need `sudo`_
-    + _Windows may need running cmd as admistrator_
++ Have [Node.js](https://nodejs.org/) installed
++ Run command **`npm install -g plcr@latest`** and you are all set
+    + To update to latest version anytime after installation, run: `npm update -g plcr`
+    + To uninstall, run: `npm uninstall -g plcr`
+    + _Unix may need `sudo` before commands_
+    + _Windows may need running cmd as administrator_
 
 # Use
 
++ Run and see: `plcr --help`
++ Example usages :
+    + `plcr -l -a https://www.youtube.com/watch?v=C0DPdy98e4c https://www.youtube.com/watch?v=ucZl6vQ_8Uo`
+    + With stdin file redirect : `plcr -p --shhh < list.txt` where `list.txt` contains :
+```https://www.youtube.com/watch?v=6kOU0uusKPE
+https://www.youtube.com/watch?v=UZSTBv4EWnU
+https://www.youtube.com/watch?v=QGraGWtktrk
+https://www.youtube.com/watch?v=B3AEQqjqr68
+https://www.youtube.com/watch?v=nKxmqgcjzAU
+https://www.youtube.com/watch?v=gOFDFogc6Gk
+https://www.youtube.com/watch?v=iE_bw4ME9v8
+https://www.youtube.com/watch?v=yzoeXkdxKuc
+https://www.youtube.com/watch?v=h11ASFLXPjg
 ```
-Usages
++ **Note**: options can not be combined
 
-    plcr url1 url2 url3 ...
-    pipe-urls-seperated-by-newlines | plcr
+---
 
-Options
-
-    NOTE: urls have to be in form https://www.youtube.com/watch/v?=...
-    NOTE: Options are not available for use with pipe
-
-    -l, --loop
-        Make playlist loop
-    -v, --version
-        Prints version
-    -h, --help
-        Print this help
-```
-
-----------------------------------------------------------------------------
-This software was not produced by or directly for YouTube, LLC and has no
-affiliation with the LLC. Use this software only at your own volition.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- plcr.js
+++ plcr.js
@@ -1,115 +1,186 @@
 #!/usr/bin/env node
 
-let URL = require('url').URL;
+const vd = require('vid_data');
 
-let playlist_link = '';
-let counter       = 0;
-let loop          = false;
+const help_text =
+`Usages:
 
-function generate(arr)
-{
-    arr.forEach((link) =>
-    {
-        if(link.match(/https:\/\/www.youtube.com\/watch\?v=.+/) !== null)
-        {
-            if(counter > 1)
-            {
-                playlist_link +=
-                    ',' + new URL(link).searchParams.get('v');
-            }
-            else if(counter === 1)
-            {
-                playlist_link +=
-                    '?playlist=' + new URL(link).searchParams.get('v');
-            }
-            else
-            {
-                playlist_link =
-                    'https://www.youtube.com/embed/'
-                    + new URL(link).searchParams.get('v');
-            }
+    plcr [options] url1 url2 url2...
 
-            ++counter;
-        }
-        else if(link === '-l' || link === '--loop')
-        {
-            loop = true;
-        }
-        else if(link === '-h' || link === '--help')
-        {
-            console.info(
-`plcr
+    -l, --loop          Set playlist to loop when ends
+    -a, --autostart     Set playlist to start playing automatically
+    -p, --pipe          Use to pipe line seperated URLs
+    -s, --shhh          Only print playlist output and nothing else
+    -v, --version       Prints running version
+    -h, --help          Display this help
 
-    Generates youtube playlist links given video urls
+Supported URL format example :
 
-Usages
+    https://www.youtube.com/watch?v=ZiXZsMIVGos
+    https://www.youtube.com/embed/ZiXZsMIVGos
+    https://youtu.be/ZiXZsMIVGos
 
-    plcr url1 url2 url3 ...
-    pipe-urls-seperated-by-newlines | plcr
+Note:
 
-Options
-
-    NOTE: urls have to be in form https://www.youtube.com/watch/v?=...
-    NOTE: Options are not available for use with pipe
-
-    -l, --loop
-        Make playlist loop
-    -v, --version
-        Prints version
-    -h, --help
-        Print this help
-
-Bugs/Fix
-
-    Repository: https://github.com/dxwc/plcr/
-    Report here: https://github.com/dxwc/plcr/issues
-
-----------------------------------------------------------------------------
-This software was not produced by or directly for YouTube, LLC and has no
-affiliation with the LLC. Use this software only at your own volition.
+- Redirecting URL list via standard input is also allowed
+- Above formatted URLs with additional path or parameters are also allowed
+- For URLs with parameters, remember to put quotation around URL to
+  prevent your terminal emulator or command line application errors
+- To update, run: npm update -g plcr
+- To uninstall, run: npm uninstall -g plcr
+- To reinstall, run: npm install -g plcr
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.`
-            );
-            process.exit(0);
-        }
-        else if(link === '-v' || link === '--version')
-        {
-            console.info('plcr 0.0.1');
-            process.exit(0);
-        }
-        else
-        {
-            console.error('SKIPPING INVALID URL/OPTION->', link);
-            console.info();
-        }
-    });
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+
+plcr version 1.0.0
+Send bug report here : https://github.com/dxwc/plcr/issues
+`;
+
+let opt =
+{
+    version   : false,
+    loop      : false,
+    autostart : false,
+    pipe      : false,
+    shhh      : false
 }
 
-if(process.argv.length > 2)
+let list = [];
+let id;
+
+process.argv.splice(0, 2);
+
+process.argv.forEach((one) =>
 {
-    generate(process.argv.slice(2));
-    playlist_link +=
-        `${counter > 1 ? '&' : '?'}rel=0${loop ? '&loop=1' : ''}&modestbranding=1`;
-    console.info();
-    console.info(playlist_link);
-}
-else
-{
-    let content = '';
-    process.stdin.resume();
-    process.stdin.setEncoding('utf-8');
-    process.stdin.on('data', (data) => content += data);
-    process.stdin.on('_socketEnd', () =>
+    one = one.toLocaleLowerCase();
+
+    if
+    (
+        one === '-v' ||
+        one === '--version'
+    )
     {
-        process.stdin.end();
-        generate(content.split('\n').filter((val) => val.length));
-        console.info();
-        console.info(playlist_link);
+        console.info('plcr 1.0.0');
+        process.exit(0);
+    }
+    else if
+    (
+        one === '-h' ||
+        one === '--help'
+    )
+    {
+        console.info(help_text);
+        process.exit(0);
+    }
+    else if
+    (
+        one === '-l'    ||
+        one === '--loop'
+    )
+    {
+        opt.loop = true;
+    }
+    else if
+    (
+        one === '-a'    ||
+        one === '--autostart'
+    )
+    {
+        opt.autostart = true;
+    }
+    else if
+    (
+        one === '-p' ||
+        one === '--pipe'
+    )
+    {
+        opt.pipe = true;
+    }
+    else if
+    (
+        one === '-s'    ||
+        one === '--sh'  ||
+        one === '--shh' ||
+        one === '--shhh'
+    )
+    {
+        opt.shhh = true;
+    }
+    else
+    {
+        process_one(one);
+    }
+});
+
+delete process.argv;
+
+if(opt.pipe) on_pipe().then(() => process_list());
+else         process_list();
+
+/////////////////////////////////////////////////////////////////////////////////
+
+function process_list()
+{
+    if(list.length === 1)
+    {
+        if(!opt.shhh) console.info(`Playlist link:\n`)
+        console.info(`https://www.youtube.com/watch?v=` + list[0]);
+    }
+    else if(list.length)
+    {
+        if(!opt.shhh) console.info(`Playlist link:\n`)
+        console.info
+        (
+            `https://www.youtube.com/embed/${list[0]}` +
+            `?playlist=${list.slice(1).join(`,`)}`     +
+            `&rel=0&modestbranding=1`                  +
+            `${opt.loop ? '&loop=1' : ''}`             +
+            `${opt.autostart ? `&autoplay=1` : '' }`
+        );
+    }
+    else
+    {
+        if(!opt.shhh) console.info(help_text);
+        process.exit(1);
+    }
+}
+
+function process_one(input)
+{
+    if(!input.trim().length) return;
+    id = vd.get_video_id(input);
+
+    if(id)             list.push(id);
+    else if(!opt.shhh) console.error(`SKIPPING INVALID OPTION/URL:`, input);
+
+    id = null;
+}
+
+function on_pipe()
+{
+    return new Promise((resolve, reject) =>
+    {
+        let content = '';
+        process.stdin.setEncoding('utf-8');
+        process.stdin.on('data', (data) => content += data);
+        process.stdin.on('end', () =>
+        {
+            content.split('\n').forEach((val) => process_one(val));
+            resolve();
+        });
+    })
+    .catch((err) =>
+    {
+        if(!opt.shhh)
+        {
+            console.error(`Unexpected error:`);
+            console.error(err);
+        }
     });
 }
